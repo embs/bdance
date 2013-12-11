f461010e04634523aa62c23e999d59c6fa4bd050
embs <embs@cin.ufpe.br>
2013-12-11 01:53:10 -0300

    Rails 4.0 default project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bdance::Application.config.secret_key_base = '2817e3efa48bf3195fe439f69b6e83e9414228bf17e9f07d250283e93635ade387a8234f3f2befade86d52979bea80714449c07b5e35095fc51931a8581b21a2'
