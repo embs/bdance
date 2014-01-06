f62e49cd4d02d922c4c52203cb00581eec5231b4
embs <embs@cin.ufpe.br>
2014-01-06 17:50:21 -0300

    Remove comentário desnecessário no modelo de User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,4 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :events
   has_many :presences
   has_one :address
-  # composed_of :birth,
-  #             :class_name => 'Date',
-  #             :mapping => %w(Date to_s),
-  #             :constructor => Proc.new { |date| (date && date.to_date) || Date.today },
-  #             :converter => Proc.new { |value| value.to_s.to_date }
 end
