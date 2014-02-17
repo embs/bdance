be3f7aba165692c1251a551dbf1d9b238aa4fee0
embs <embs@cin.ufpe.br>
2014-02-16 23:33:41 -0300

    Adiciona campos obrigatórios para criação de usuário no formulário de cadastro e de edição de conta do Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,22 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   check_authorization unless: :devise_controller?
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, alert: exception.message
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:email, :password, :password_confirmation, :first_name, :last_name,
+        :birth, :username)
+    end
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:email, :password, :password_confirmation, :first_name, :last_name,
+        :birth, :username, :current_password)
+    end
   end
 end
